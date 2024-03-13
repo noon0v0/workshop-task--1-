4a90e24ea2f8aee69c15149d6858cffb5d1ab36b
noon0v0 <162386469+noon0v0@users.noreply.github.com>
2024-03-13 13:37:31 +1300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 function setup() {
   createCanvas(500, 500);
-  background(220);
+  background(250, 220, 110);
   for (x = 0; x < 30; x += 2){
     lollipop(random(100,width - 20), random(230,height - 30), random(3, 150), color(255, 120, 100))
   }
@@ -9,7 +9,7 @@ function setup() {
 function lollipop(x, y, size, flavour){
   fill(30, 255, 200);
   noStroke();
-  arc(x, y - size, 60, 30, 150, PI + QUARTER_PI, CHORD)
+  arc(x, y - size, 60, 30, 190, PI + QUARTER_PI, CHORD)
   fill(flavour);
   ellipseMode(CORNER);
   ellipse(x - 60, y - size - 70, 30, 50);
